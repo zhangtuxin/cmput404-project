e1cd2db4b74c779fe473f1dac1d3d2945f65cd8d
Nunchakus Lei <jimmynunchakuslei@gmail.com>
2017-02-28 00:14:20 -0700

    initial commit for TSK-1-2

--- cmput404_project/urls.py
+++ cmput404_project/urls.py
@@ -17,6 +17,6 @@ from django.conf.urls import include,url
 from django.contrib import admin
 
 urlpatterns = [
-    url('^',include("django.contrib.auth.urls")),
+    url(r'^auth',include("django.contrib.auth.urls")),
     url(r'^admin/', admin.site.urls),
 ]
